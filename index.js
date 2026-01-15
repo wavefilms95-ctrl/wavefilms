d2638978951348380cb93be8b9b230f35cf95d50
wavefilms95-ctrl <wavefilms95@gmail.com>
2026-01-14 23:58:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const urls = [
-    <img src="https://image.tmdb.org/t/p/w500/1qpUkVLQ1ItgKcwAJTaEkz5uAjt.jpg" alt="Dune 2">
-<img src="https://image.tmdb.org/t/p/w500/8cdWjvZQUExUUTzyp4t6EDMUBj4.jpg" alt="Spider-Man">
-<img src="https://image.tmdb.org/t/p/w500/1Lhc32tqZ9J14fAepUl0fYd1iHg.jpg" alt="Oppenheimer">
-<img src="https://image.tmdb.org/t/p/w500/4m1MWRmE5Z2kpf6pbI2lLlKmol2.jpg" alt="Deadpool & Wolverine">
-<img src="https://image.tmdb.org/t/p/w500/9xn4v4Liib2tMI7i0kpxnBtqr5j.jpg" alt="Interstellar">
-<img src="https://image.tmdb.org/t/p/w500/kDp1vUWxXjK3vCW4AoD7QK5pqwj.jpg" alt="The Matrix">
+ 
     'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3105be1aedb18e8d02e7ad7a1b180bef',
     'https://api.themoviedb.org/3/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&api_key=3105be1aedb18e8d02e7ad7a1b180bef',
     'https://api.themoviedb.org/3/discover/movie?with_genres=18&primary_release_year=2014&api_key=3105be1aedb18e8d02e7ad7a1b180bef'
+    'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6e09b38cb67e355becb29d99d739c231',
+    'https://api.themoviedb.org/3/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&api_key=6e09b38cb67e355becb29d99d739c231',
+    'https://api.themoviedb.org/3/discover/movie?with_genres=18&primary_release_year=2014&api_key=6e09b38cb67e355becb29d99d739c231'
 ];
 window.addEventListener('DOMContentLoaded',()=>{
     const peticiones = urls.map(peticion=>fetch(peticion));
